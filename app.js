1055920bf7de18244c2599e707b2131d30a70877
nickgoodrow <nickgoodrow@gmail.com>
2017-02-17 19:54:05 -0500

    Added post and comment model, controller and routes

--- app.js
+++ app.js
@@ -3,10 +3,11 @@ var mongoose = require('mongoose');
 var bodyParser = require('body-parser');
 
 // open connection to DB, passing in mongodb conn string => like a dbContext
-var db = mongoose.connect('mongodb://localhost:12345/bookApi');
+var db = mongoose.connect('mongodb://localhost:12345/travelpost');
 
 // this return our book model hook up to mongoose that gets form mongoDb
-var Book = require('./models/bookModel');
+var Post = require('./models/postModel');
+var Comment = require('./models/commentModel');
 
 //  express instance
 var app = express();
@@ -19,10 +20,12 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 // require book routes file
-var bookRouter = require('./routes/bookRoutes')(Book);// exec () to return bookRouter
+var postRouter = require('./routes/postRoutes')(Post);// exec () to return bookRouter
+var commentRouter = require('./routes/commentRoutes')(Comment);// exec () to return bookRouter
 
 // define URLs
-app.use('/api/books', bookRouter);
+app.use('/api/posts', postRouter);
+app.use('/api/comments', commentRouter);
 //app.use('/api/authors', authorRouter); for more...
 
 // setup a handler for a route, when it hits the root
